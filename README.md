e5890eef8836c95bbda4b1f292dc59185db60e7b
Romain Madar <romain.madar@cern.ch>
2018-08-03 14:42:16 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# High quality HEP distribution
+
+This code allows to produce, in a very flexible way, publication quality HEP distributions
+based on ROOT histograms.
+
+## Installation
+
+```
+git clone https://github.com/rmadar/hepplotting
+cd hepplotting
+pip install -e . -user
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hepplotting',
+      version='0.1',
+      description='Make publication quality plot for HEP based on ROOT histograms',
+      packages=['hepplotting'],
+      zip_safe=False,
+      license='MIT',
+      author_email='romain.madar@cern.ch',
+      author='Romain Madar',
+)
